ce15457a2b0f1582e6332b9faf8193985c381847
JinyongShin <jysin0102@gmail.com>
2021-06-23 14:26:19 +0900

    Revised (Selecting machines added)

--- make_image.sh
+++ make_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat << EOF > runCondorBase_${1%.*}.sh
+cat << EOF > runCondorIm_${1%.*}.sh
 #!/bin/bash
 
 filename=\`basename \${1}\`
@@ -21,30 +21,30 @@ unset __conda_setup
 
 $(echo 'EOF')
 
-if [ ! -d condorBaseOut ]; then mkdir condorBaseOut; fi
+if [ ! -d condorImOut ]; then mkdir condorImOut; fi
 
-python /x4/cms/jyshin/HDT_Classification/Baseline_Selection_v4.py \${1} -o \${process}
+python /x4/cms/jyshin/HDT_Classification/make_image_v2.py \${1} -o \${process}
 
-mv *.npy condorBaseOut/
+mv *.h5 condorImOut/
 EOF
 
-chmod +x runCondorBase_${1%.*}.sh
+chmod +x runCondorIm_${1%.*}.sh
 
 cat << EOF > job_${1%.*}.jdl
-executable = runCondorBase_${1%.*}.sh
+executable = runCondorIm_${1%.*}.sh
 universe = vanilla
-output   = condorBaseLog/condorBaseLog.log
-error    = condorBaseLog/condorBaseErr.err
+output   = condorImLog/condorImLog.log
+error    = condorImLog/condorImErr.err
 log      = /dev/null
 getenv = True
 should_transfer_files = yes
 when_to_transfer_output = ON_EXIT
-transfer_output_files = condorBaseOut
+transfer_output_files = condorImOut
 requirements = (machine == "node01")||(machine == "node02")||(machine == "node03")||(machine == "node04")||(machine=="node05")
 arguments = \$(DATAFile) 
 queue DATAFile from ${1}
 EOF
-if [ ! -d condorBaseLog ]; then mkdir condorBaseLog; fi
+if [ ! -d condorImLog ]; then mkdir condorImLog; fi
 
 condor_submit job_${1%.*}.jdl
 
